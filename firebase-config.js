ededb8b4de5d5ac31c8cad22816eba5a481f13c7
HastianTorres <142507148+HastianTorres@users.noreply.github.com>
2026-06-30 22:09:21 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCa8GRUWxwm6PN9stQo0WRVDLGcdyuc",
-  authDomain: "controle-remedios-mae.firebaseapp.com",
-  projectId: "controle-remedios-mae",
-  storageBucket: "controle-remedios-mae.firebasestorage.app",
-  messagingSenderId: "325439951301",
-  appId: "1:325439951301:web:21400bb28d71b68ccf3bac"
-};
